9c3dbb34e399c505f0529c2a6bb28cb6b1bfc707
unknown <deepaksaivenkat1111@gmail.com>
2024-05-21 20:26:05 +0530

    ensuring pip

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,11 @@
 # build_files.sh
 
-python3.9 -m pip install --upgrade pip
-
-pip install -r requirements.txt
+#!/bin/bash
+# Ensure pip is available
+/usr/bin/python3.9 -m ensurepip --upgrade
+# Upgrade pip
+/usr/bin/python3.9 -m pip install --upgrade pip
+# Install dependencies from requirements.txt
+/usr/bin/python3.9 -m pip install -r requirements.txt
+# Add other necessary build steps below
 python3.9 manage.py collectstatic --noinput
