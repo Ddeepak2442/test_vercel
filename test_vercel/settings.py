995517031468dccbaad1e3bdc1fedd8d7ff3fb3d
unknown <deepaksaivenkat1111@gmail.com>
2024-05-21 20:34:38 +0530

    static in settings fixed

--- test_vercel/settings.py
+++ test_vercel/settings.py
@@ -110,12 +110,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
