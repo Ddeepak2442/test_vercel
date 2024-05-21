e22d2bf704de60676ca8a83cc117bd8a05f24c77
unknown <deepaksaivenkat1111@gmail.com>
2024-05-21 20:29:17 +0530

    static removed

--- build_files.sh
+++ build_files.sh
@@ -8,4 +8,4 @@
 # Install dependencies from requirements.txt
 /usr/bin/python3.9 -m pip install -r requirements.txt
 # Add other necessary build steps below
-python3.9 manage.py collectstatic --noinput
+#python3.9 manage.py collectstatic --noinput
